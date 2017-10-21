46f9798c2fc77001c80be07fad73537ecee88a11
cion94 <cristian.ion94@gmail.com>
2017-10-22 00:59:14 +0300

    chat init

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.get("/login", function(req, res){
     res.render("login");
 });
 
+app.get("/chat", function(req, res){
+    res.render("chat")
+});
+
 app.get("/register", function(req, res){
     res.render("register");
 });
@@ -32,12 +36,33 @@ app.post("/logout", function(req, res){
     res.render("head");
 });
 
-app.listen(port, function(err){
+
+const server = app.listen(port, function(err){
     if(err){
         console.log("[ERR]Could not listen on " + port, err)
     }
     else{
         console.log("Listening on port " + port);
     }
+});
+
+const io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    socket.broadcast.emit('chat message', msg);
+  });
+
+  /*socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });*/
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
 });
 
